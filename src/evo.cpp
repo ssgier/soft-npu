8d03c56b9ef73fcc565c238bce089bcece6582e9
ssgier <sandro.sgier@gmail.com>
2022-05-17 17:53:51 +0200

    Program options to reproduce/resume

--- src/evo.cpp
+++ src/evo.cpp
@@ -5,16 +5,16 @@
 #include <plog/Init.h>
 #include <plog/Log.h>
 #include <experiments/EvolutionWrapper.hpp>
+#include <experiments/POCDynamicSimulation.hpp>
 
 using namespace plog;
 using namespace soft_npu;
 
-int main() {
+void runEvolution(const std::string& paramsTemplatePath) {
     ConsoleAppender<plog::TxtFormatter> consoleAppender;
-
     plog::init(plog::info, &consoleAppender);
 
-    PLOG_INFO << "Application starting";
+    PLOG_INFO << "Running evolution, params template file path: " << paramsTemplatePath;
 
     auto paramsTemplate = ParamsType::parse(FileUtil::getFileContent(
         "../resources/paramsTemplate.json"));
@@ -29,5 +29,33 @@ int main() {
     std::ofstream fout("../resources/outParams.json");
     fout << outParams.evolvedParams->dump(4);
     fout.close();
+}
+
+void runRepro() {
+    ConsoleAppender<plog::TxtFormatter> consoleAppender;
+    plog::init(plog::debug, &consoleAppender);
+
+    PLOG_INFO << "Running reproduction";
+
+    auto params = std::make_shared<ParamsType>(ParamsType::parse(FileUtil::getFileContent(
+            "../resources/outParams.json")));
+
+    POCDynamicSimulation simulation(params);
+
+    auto simulationResult = simulation.run();
+    PLOG_INFO << simulationResult;
+}
+
+int main(int argc, char * argv[]) {
+
+    if (argc == 1) {
+        runEvolution("../resources/paramsTemplate.json");
+    } else if (argc == 2 && strcmp("--resume", argv[1]) == 0) {
+        runEvolution("../resources/outParams.json");
+    } else if (argc == 2 && strcmp("--repro", argv[1]) == 0) {
+        runRepro();
+    } else {
+        throw std::runtime_error("Invalid program arguments");
+    }
 }
 
